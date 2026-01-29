4b797bd4b4e1e34a4c638a0a19f80cfa5cab147e
inkB12 <ttinhbao@gmail.com>
2026-01-29 11:11:20 +0700

    [UPDATE] Update responsefilter + middleware

---
+++ TetGift.BLL/Common/ApiResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace TetGift.BLL.Common
+{
+    public class ApiResponse<T>
+    {
+        public int Status { get; set; }
+        public string Msg { get; set; } = "";
+        public T? Data { get; set; }
+    }
+}
